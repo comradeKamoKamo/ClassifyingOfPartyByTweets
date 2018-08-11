ee5fec26292e5dc14f57ef411e61f397cd3317c4
T.T <tamutamuphone@gmail.com>
2018-08-11 16:04:24 +0900

    DPFP追加漏れ

--- DataCollecting/data_collect.py
+++ DataCollecting/data_collect.py
@@ -3,12 +3,12 @@ import csv
 from collections import OrderedDict
 from path import Path
 
-with open("Politicians.csv","r") as f:
+with open("DataCollecting/Politicians.csv","r") as f:
     parties = csv.DictReader(f)
 
-    get_tweets = GetTweets.GetTweets("OAuth.json")
+    get_tweets = GetTweets.GetTweets("DataCollecting/OAuth.json")
 
     #実際にはAPIの制限があるので途中で止まる。その場合は続きから。
     for party in parties:
         get_tweets.get_tweets(party["screen_name"])
-        get_tweets.save_tweets(Path("rawdata/" + party["party_name"] + ".csv"))
+        get_tweets.save_tweets(Path("DataCollecting/rawdata/" + party["party_name"] + ".csv"))
--- train.py
+++ train.py
@@ -19,12 +19,13 @@ def main():
     y_train_ = utils.np_utils.to_categorical(y_train,n_classes)
     X_test , y_test = np_load("Data/test.npz")
     y_test_ = utils.np_utils.to_categorical(y_test,n_classes)
-    """
+    
     model = build_model(PART_SIZE)
     model = train(model,X_train,y_train_,X_test,y_test_)
-    """
     
+    """
     model = load_model("model.json","model.hdf5")
+    """
     test(model,X_test,y_test)
     
 
