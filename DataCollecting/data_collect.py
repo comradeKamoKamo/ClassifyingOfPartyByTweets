cc7f5d8adba624faac736ebba63f740d875bffeb
KamoKamo <tamutamuphone@gmail.com>
2018-09-13 16:50:50 +0900

    API規制回避を正しく

--- DataCollecting/data_collect.py
+++ DataCollecting/data_collect.py
@@ -8,7 +8,7 @@ with open("DataCollecting/Politicians.csv","r") as f:
 
     get_tweets = GetTweets.GetTweets("DataCollecting/OAuth.json")
 
-    #実際にはAPIの制限があるので途中で止まる。その場合は続きから。
+    #GetTweets.pyを修正したのでAPI規制は回避できる。遅くなるけど。
     for party in parties:
-        get_tweets.get_tweets(party["screen_name"])
+        get_tweets.get_tweets(party["screen_name"],avoid_api_regulation=True)
         get_tweets.save_tweets(Path("DataCollecting/rawdata/" + party["party_name"] + ".csv"))
