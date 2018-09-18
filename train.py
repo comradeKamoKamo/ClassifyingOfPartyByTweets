44b64b90f9dfee7b4176e7b1aa3d2aba63f237a8
KamoKamo <tamutamuphone@gmail.com>
2018-09-19 07:19:54 +0900

    3回め。

--- train.py
+++ train.py
@@ -21,11 +21,11 @@ def main():
     y_test_ = utils.np_utils.to_categorical(y_test,n_classes)
     
     
-    #model = build_model(PART_SIZE)
-    #model = train(model,X_train,y_train_,X_test,y_test_)
+    model = build_model(PART_SIZE)
+    model = train(model,X_train,y_train_,X_test,y_test_)
     
     
-    model = load_model("model.json","model.hdf5")
+    #model = load_model("model.json","model.hdf5")
 
     test(model,X_test,y_test)
     
