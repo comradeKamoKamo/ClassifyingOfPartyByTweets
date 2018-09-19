4b92bafd9472b6c5f10842bea2409bb3bebdbc7e
KamoKamo <tamutamuphone@gmail.com>
2018-09-19 17:42:14 +0900

    LFS規制回避

---
+++ auto.bat
@@ -0,0 +1,6 @@
+python Data/split_train_and_test.py
+python Data/create_empty_db.py
+python Data/insert_values_to_db.py
+python Data/insert_standard_to_db.py
+python Data/data_to_npz.py
+python train.py
